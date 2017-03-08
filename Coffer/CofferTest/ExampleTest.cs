e58f2f7fca4a1e9a32826d9fbf9f27d378568e1e
giuli <neoisgodlike@hotmail.com>
2017-03-08 23:10:00 +0100

    added test unit test project

---
+++ Coffer/CofferTest/ExampleTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CofferTest
+{
+    [TestClass]
+    public class ExampleTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ExampleTestMethod()
+        {
+            throw new ArgumentOutOfRangeException("bumm");
+        }
+    }
+}
