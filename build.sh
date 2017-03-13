44e064e93d9c0a3fae7f46fdedf3b470d469657f
giuliboy <giuliano.degani@gmail.com>
2017-03-13 10:47:31 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 dotnet restore
-dotnet test ./CofferTest/CofferTest.csproj -c Release -f netcoreapp1.0
+dotnet test ./CofferTest/CofferTest -c Release -f netcoreapp1.0
 dotnet build **/project.json
  
